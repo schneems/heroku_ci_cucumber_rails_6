cc4f7b20eedc266c91d8c9a939d44bb64152e03b
schneems <richard.schneeman+foo@gmail.com>
2019-06-04 16:34:17 -0500

    Heroku find chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -56,10 +56,25 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+if ENV['HEROKU_TEST_RUN_BRANCH']
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+  chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+       app,
+       browser: :chrome,
+       desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+    )
+  end
+
+  Capybara.javascript_driver = :chrome
+else
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+
+  Capybara.javascript_driver = :chrome
 end
-
-Capybara.javascript_driver = :chrome
-
